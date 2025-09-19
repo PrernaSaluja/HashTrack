ea1ecbc2c143bb2eb0fb5ef69bf9de8dafb69570
PrernaSaluja <142964245+PrernaSaluja@users.noreply.github.com>
2025-09-19 20:54:24 +0530

    Create Project.test.js

---
+++ test/Project.test.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+
+describe("Project contract", function () {
+    let project, owner;
+
+    beforeEach(async () => {
+        const Project = await ethers.getContractFactory("Project");
+        [owner] = await ethers.getSigners();
+        project = await Project.deploy();
+    });
+
+    it("Should upload and retrieve a file", async function () {
+        await project.uploadFile("QmHash123", "file.txt");
+
+        const files = await project.getMyFiles();
+        expect(files.length).to.equal(1);
+        expect(files[0].fileHash).to.equal("QmHash123");
+    });
+});
