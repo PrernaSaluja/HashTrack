ca19aa17d199b72b3910bab75c7a4946c91c0781
PrernaSaluja <142964245+PrernaSaluja@users.noreply.github.com>
2025-11-04 00:51:00 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
-    solidity: "0.8.20",
-    networks: {
-        sepolia: {
-            url: process.env.RPC_URL,
-            accounts: [process.env.PRIVATE_KEY]
-        }
-    }
-}; 
+  solidity: "0.8.17",
+  networks: {
+    coreTestnet: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 1114,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+};
