1ff83a072942bf29f6a9be532d33ae90c0530df6
PrernaSaluja <142964245+PrernaSaluja@users.noreply.github.com>
2025-11-13 10:55:22 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
     const Project = await hre.ethers.getContractFactory("Project");   
-    const project = await Project.deploy();
+    const project = await Project.deploy(); 
     await project.deployed();
 
     console.log(`Project deployed to: ${project.address}`);
